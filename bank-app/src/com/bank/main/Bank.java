d7d08a277cb8bd61c9bc059852dfbaf80688a0b2
J Shim <shimjay1@gmail.com>
2019-02-22 11:01:46 -0500

    changed bank to a singleton class

--- bank-app/src/com/bank/main/Bank.java
+++ bank-app/src/com/bank/main/Bank.java
@@ -2,19 +2,28 @@ package com.bank.main;
 
 public class Bank {
 	
+	private static Bank bank;
+	
 	private Handler handler;
 	private Application app;
 	
-	public Bank() {
+	private Bank() {
 		
 		handler = new Handler();
 		app = new Application(handler);
 		
 		app.start();
 	}
+	
+	public static Bank getInstance() {
+		if(bank == null) {
+			bank = new Bank();
+		}
+		return bank;
+	}
 
 	public static void main(String[] args) {
-		new Bank();
+		Bank bank = Bank.getInstance();
 	}
 
 }
